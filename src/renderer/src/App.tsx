3636c58c9adb8fca786c7081d05f79f7fb865c80
HiMyNameisScott <50992532+HiMyNameisScott@users.noreply.github.com>
2026-03-12 16:38:11 -0700

    Added comments to routes
    Comments to routes to explain functionality and that they tie to mvp.  didn't label them specifically but referring to the working document it should be fairly clear which goes with which.

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -4,6 +4,18 @@ import Login from "./pages/Login"
 import Dashboard from "./pages/Dashboard"
 import Grading from "./pages/Grading"
 
+
+// Routes work functionally by iumnporting the page to the top, and then creating the route below. What this will do is give you the ability to create
+// a new .tsx inside of pages, and then you can create buttons/navigation that point from whatever page you're on to this route the route will then 
+// go to the appropriate page/.tsx file. 
+
+// note some things such as "/" is always home/root this is a design paradigm
+
+// As far as the routes go individually, these point to the mvps that we have currently.
+// login -> login/landing
+// dashboard -> is the student/teacher ui (these should definitely be different routes/pages)
+// grading -> is the grade book, and should also probably be separated into grading.teacher / grading.student.
+
 function App(): React.JSX.Element {
   return (
     <HashRouter>
